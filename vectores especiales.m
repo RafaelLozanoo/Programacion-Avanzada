f844a46387660b87ec28be648e3ed9a978622c10
Rafael Lozano <rafalozano284@gmail.com>
2020-06-03 13:03:43 -0500

    Cambio de nombre y primer codigo.

---
+++ vectores especiales.m
@@ -0,0 +1,6 @@
+clear all 
+close all
+clc 
+%% Vectores 
+A=round (rand(1,10)*500) %Defino A como un numero redondo o entero, si decimales,rellenando el vector con numeros aleatrorios de 0 a 500
+B=rand (1,10)+100
