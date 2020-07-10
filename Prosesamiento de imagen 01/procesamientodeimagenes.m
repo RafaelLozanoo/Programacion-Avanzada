298df255b677e501cf323ed2d903e63a45ed050b
Rafael Lozano <rafalozano284@gmail.com>
2020-07-09 20:10:08 -0500

    Imagen agregada

--- Prosesamiento de imagen 01/procesamientodeimagenes.m
+++ Prosesamiento de imagen 01/procesamientodeimagenes.m
@@ -3,19 +3,19 @@ clear all
 clc
 %%
 %imread('C:\Users\rafal\Documents\GitHub\Programacion-Avanzada\Prosesamiento de imagen 01\IMG01.jpg');
-% imagen=imread('IMG01.jpg');
-% imshow(imagen);
-% %%separar una imagen en sus tres canales
-% imr=imagen(:,:,1); % Canal 1 -- R
-% img=imagen(:,:,2); % Canal 2 -- G
-% imb=imagen(:,:,3); % Canal 3 -- B
-% figure;
-% 
-% imshow(imr); title('Imagen canal Rojo');
-% figure;
-% imshow(img); title('Imagen canal Verde');
-% figure;
-% imshow(imb); title('Imagen canal Azul');
+imagen=imread('IMG01.jpg');
+imshow(imagen);
+%%separar una imagen en sus tres canales
+imr=imagen(:,:,1); % Canal 1 -- R
+img=imagen(:,:,2); % Canal 2 -- G
+imb=imagen(:,:,3); % Canal 3 -- B
+figure;
+
+imshow(imr); title('Imagen canal Rojo');
+figure;
+imshow(img); title('Imagen canal Verde');
+figure;
+imshow(imb); title('Imagen canal Azul');
 %% Imagen va de colores de 0 a 255
 %0 --> Negro
 %255 --> Blanco puro
